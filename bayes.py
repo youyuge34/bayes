9cfe53f8b1ca4ad830627a0fd9ce397a1f26216d
Andy <1197993367@qq.com>
2017-09-04 10:23:22 +0800

    4.7 使用朴素贝叶斯分类器从个人广告中获取区域倾向

--- bayes.py
+++ bayes.py
@@ -4,7 +4,7 @@
 @contact: 1197993367@qq.com
 @site: http://youyuge.cn
 
-@version: 1.0
+@version: 1.txt.0
 @license: Apache Licence
 @file: bayes.py
 @time: 17/9/3 上午9:07
@@ -19,7 +19,7 @@ def loadDataSet():
                    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
                    ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
+    classVec = [0, 1, 0, 1, 0, 1]  # 1.txt is abusive, 0 not
     return postingList, classVec
 
 def createVocabList(dataSet):
@@ -150,3 +150,97 @@ def spamTest():
             print "classification error", docList[docIndex]
     print 'the error rate is: ', float(errorCount) / len(testSet)
     # return vocabList,fullText
+
+
+def calcMostFreq(vocabList, fullText):
+    """
+    高频词汇去除函数
+    :param vocabList:
+    :param fullText:
+    :return:
+    """
+    import operator
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedFreq[:30]
+
+
+def calcStopWordList():
+    """
+    利用停用词表移除辅助词
+    :return:
+    """
+    import codecs
+    file = codecs.open('1.txt', 'r', 'utf8')
+    lines = [line.strip() for line in file]
+    file.close()
+    return lines
+
+
+def localWords(feed1, feed0):
+    """
+    RSS源分类器
+    :param feed1:
+    :param feed0:
+    :return:
+    """
+    import feedparser
+    docList = [];
+    classList = [];
+    fullText = []
+    minLen = min(len(feed1['entries']), len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)  # NY is class 1.txt
+        wordList = textParse(feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)  # create vocabulary
+    # top30Words = calcMostFreq(vocabList,fullText)   #remove top 30 words
+    # for pairW in top30Words:
+    #     if pairW[0] in vocabList: vocabList.remove(pairW[0])
+    stopList = calcStopWordList()  # 使用停用词表去除辅助词
+    for word in stopList:
+        if word in vocabList: vocabList.remove(word)
+    trainingSet = range(2 * minLen);
+    testSet = []  # create test set
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+    trainMat = [];
+    trainClasses = []
+    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+        trainMat.append(words2VecBag(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:  # classify the remaining items
+        wordVector = words2VecBag(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+    print 'the error rate is: ', float(errorCount) / len(testSet)
+    return vocabList, p0V, p1V
+
+
+def getTopWords(ny, sf):
+    import operator
+    vocabList, p0V, p1V = localWords(ny, sf)
+    topNY = [];
+    topSF = []
+    for i in range(len(p0V)):
+        if p0V[i] > -4.5: topSF.append((vocabList[i], p0V[i]))
+        if p1V[i] > -4.5: topNY.append((vocabList[i], p1V[i]))
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**"
+    for item in sortedSF:
+        print item[0]
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
+    for item in sortedNY:
+        print item[0]
--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 @contact: 1197993367@qq.com
 @site: http://youyuge.cn
 
-@version: 1.0
+@version: 1.txt.0
 @license: Apache Licence
 @file: test.py
 @time: 17/9/3 上午9:28
@@ -19,16 +19,20 @@ def run():
     myVocabList = bayes.createVocabList(postingList)
     # print myVocabList
     # print bayes.words2Vec(myVocabList,postingList[0])
-    trainMat = []
-    for postinDoc in postingList:
-        trainMat.append(bayes.words2Vec(myVocabList, postinDoc))
-    p0V, p1V, pAb = bayes.trainNB0(trainMat, classVec)
+    # trainMat = []
+    # for postinDoc in postingList:
+    #     trainMat.append(bayes.words2Vec(myVocabList, postinDoc))
+    # p0V, p1V, pAb = bayes.trainNB0(trainMat, classVec)
     # bayes.testingNB()
-    bayes.spamTest()
+    # bayes.spamTest()
     # print pAb
     # print p0V
     # print p1V
-
+    import feedparser
+    ny = feedparser.parse('https://newyork.craigslist.org/search/res?format=rss')
+    sf = feedparser.parse('https://sfbay.craigslist.org/search/apa?format=rss')
+    # bayes.localWords(ny,sf)
+    bayes.getTopWords(ny, sf)
 
 if __name__ == '__main__':
     run()
